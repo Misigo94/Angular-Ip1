07d19f94e8a8401a6fb3a78e82ef03866d0ba2c1
Misigomartin <Misigomartin@gmail.com>
2022-04-10 18:21:38 +0300

    create a new directive

---
+++ src/app/highlight-quote.directive.spec.ts
@@ -0,0 +1,17 @@
+import { HighlightQouteDirective } from './highlight-quote.directive';
+
+describe('HighlightQuoteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightQouteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
+
+// import { HighlightQouteDirective } from './highlight-qoute.directive';
+
+// describe('HighlightQouteDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new HighlightQouteDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
