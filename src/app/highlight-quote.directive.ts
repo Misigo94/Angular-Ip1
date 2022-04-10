f5bcabc796c22ae3ce9eaebaa45a5b9914c9df35
Misigomartin <Misigomartin@gmail.com>
2022-04-10 18:21:54 +0300

    create a new directive

---
+++ src/app/highlight-quote.directive.ts
@@ -0,0 +1,35 @@
+// import { Directive,ElementRef,HostListener } from '@angular/core';
+
+// @Directive({
+//   selector: '[appHighlightQuote]'
+// })
+// export class HighlightQuoteDirective {
+
+//   constructor(private elem: ElementRef) { }
+//   @HostListener("click") onClicks() {
+//     this.textDeco("green")
+//   }
+
+// }
+
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightQoute]'
+})
+export class HighlightQouteDirective {
+
+  constructor(private elem:ElementRef) { 
+  }
+  @HostListener("click") onClicks(){
+    this.textDeco("green")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
+
+}
