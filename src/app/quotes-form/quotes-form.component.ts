36667e57eb05f1b66458aa8c4508851255b89f03
Misigomartin <Misigomartin@gmail.com>
2022-04-10 19:29:10 +0300

    reduce number of quites

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+// import { DatePipe } from '@angular/common';
+// import { Component, OnInit, Output, EventEmitter, } from '@angular/core';
+// import { Quote } from '../quote';
+
+// @Component({
+//   selector: 'app-quotes-form',
+//   templateUrl: './quotes-form.component.html',
+//   styleUrls: ['./quotes-form.component.css']
+// })
+// export class QuotesFormComponent implements OnInit {
+//    newQuote = new Quote( 0,"", "", "" ,0,0);
+//   @Output() addQuote = new EventEmitter<Quote>();
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+// }
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,10 +26,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+  quoted = new Quote(0,"","","","",new Date(),0,0);
+  @Output() add = new EventEmitter<Quote>();
 
+  addQuote(){
+this.add.emit(this.quoted);
+this.quoted = new Quote(0,"","","","",new Date(),0,0);
+  }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
