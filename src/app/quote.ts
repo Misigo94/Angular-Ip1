bed6922fe8f574a246f5e1362797b5705f756e50
Misigomartin <Misigomartin@gmail.com>
2022-04-10 20:23:44 +0300

    make changes to class Quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,30 @@
+// export class Quote {
+//      id: number;
+//     name: string;
+//     quote: string;
+//     author: string;
+//      datePosted:any;
+//      upVotes: number;
+//     downVotes: number;
+//   likes: number;
+//   dislikes: number;
+
+//     constructor( id: number,name: string,quote: string,
+//         author: string, upVotes: number, downVotes: number) {
+//         this.id = id;
+//         this.name = name;
+//         this.quote = quote;
+//         this.author = author;
+//         this.upVotes = upVotes;
+//         this.downVotes = downVotes;
+// }
+
+// }
+
+
 export class Quote {
-    name: string;
-    vote: number;
-    description: string;
-    
+  showInfo: boolean;
+  constructor(public id:number,public name:string,public title:string,public quote:string,public author:string, public datePosted: Date,public likes:number, public dislikes:number){
+      this.showInfo=false;
+    }
 }
